a6fbfbf47bb9aa48c8f62c18e768dc6b2ee9f407
TOMIKAWA Sotaro <sotaro.tomikawa@zozo.com>
2024-07-29 14:16:20 +0900

    feat: make fixable

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 function reporter(context) {
-  const { Syntax, report, RuleError } = context;
+  const { Syntax, report, fixer, RuleError } = context;
   return {
     [Syntax.Header](node) {
       if (node.depth !== 1) return;
-      report(node, new RuleError("Use h2(`##`) instead of h1(`#`)"));
+      const hashH1 = node.raw.match(/^#\s/)
+      const equalH1 = node.raw.match(/\n\s*(=+)\s*$/)
+      if (hashH1) {
+        // fixable
+        const fix = fixer.insertTextBefore(node, '#');
+        report(node, new RuleError("Use h2(`##`) instead of h1(`#`)", { fix }));
+        return;
+      }
+      if (equalH1){
+        // fixable
+        const fix = fixer.replaceText(node, node.raw.replace(/=+\s*$/, (matched) => matched.replace(/=/g, '-')));
+        report(node, new RuleError("Use h2 instead of h1", { fix }));
+        return;
+      }
+      report(node, new RuleError("Use h2 instead of h1"));
     },
   };
 }
-module.exports = { linter: reporter };
+module.exports = { linter: reporter, fixer: reporter };
